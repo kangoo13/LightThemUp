0f5b0536c2f1521e03182c62ce885a338f1e1f83
aurelienschiltz <aurelien.schiltz@epitech.eu>
2016-05-04 23:38:22 +0800

    Frontend start

---
+++ Frontend/app.js
@@ -0,0 +1,67 @@
+
+/**
+ * Module dependencies
+ */
+
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('error-handler'),
+  morgan = require('morgan'),
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  http = require('http'),
+  path = require('path');
+
+var app = module.exports = express();
+
+
+/**
+ * Configuration
+ */
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var env = process.env.NODE_ENV || 'development';
+app.use(function(err, req, res, next) {
+  return res.status(500).send({
+    success: false,
+    message: err.toString(),
+  });
+});
+
+// production only
+if (env === 'production') {
+  // TODO
+}
+
+
+/**
+ * Routes
+ */
+
+// serve index and view partials
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+app.get('/api/name', api.name);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+
+/**
+ * Start Server
+ */
+
+http.createServer(app).listen(80, function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
--- Frontend/application/views/templates/header.php
+++
@@ -1,7 +0,0 @@
-<html>
-        <head>
-                <title>CodeIgniter Tutorial</title>
-        </head>
-        <body>
-
-                <h1><?php echo $title; ?></h1>
