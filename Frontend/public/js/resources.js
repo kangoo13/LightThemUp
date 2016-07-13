e91bbddf5726420e843e3c1a084a104972a80a9b
Tanguy Devos <contact@tan-devos.fr>
2016-07-13 08:00:52 -0400

    ss

--- Frontend/public/js/resources.js
+++ Frontend/public/js/resources.js
@@ -445,3 +445,24 @@ app.directive('back', ['$window', function($window) {
 		}
 	};
 }]);
+
+var compareTo = function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+             
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+ 
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+};
+ 
+app.directive("compareTo", compareTo);
