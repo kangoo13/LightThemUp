cd1ab3d3153530c521190aa53e81daab6d439f85
tanguydevos <contact@tan-devos.fr>
2016-09-23 09:54:45 +0200

    fixes

--- API/models/Achievement.js
+++ API/models/Achievement.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
 var AchievementSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    picture: {type: String, default: "uploads/news/default-achievement.png"}
+	name: String,
+	description: String,
+	picture: {type: String, default: "uploads/achievements/default-achievement.png"}
 },
-    {
-        timestamps: true
-    });
+{
+	timestamps: true
+});
 
 module.exports = mongoose.model('Achievement', AchievementSchema);
