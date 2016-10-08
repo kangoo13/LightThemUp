eb0e39785c8cb9234f8c197a8fca1bbfecbbc6a4
tanguydevos <contact@tan-devos.fr>
2016-10-08 14:09:12 +0200

    update playlist model

--- API/models/Playlist.js
+++ API/models/Playlist.js
@@ -1,14 +1,14 @@
- var mongoose    = require('mongoose');
- var Schema      = mongoose.Schema;
+var mongoose    = require('mongoose');
+var Schema      = mongoose.Schema;
 
- var PlaylistSchema = new mongoose.Schema({
-     name: String,
- 	 created_by: String, 
-     slug: String,
- 	 songs: [{type: Schema.Types.ObjectId, ref: 'Song'}]
- },
- {
-     timestamps: true
- });
+var PlaylistSchema = new mongoose.Schema({
+  name: String,
+  created_by: String,
+  slug: String,
+  songs: [{type: Schema.Types.ObjectId, ref: 'Song'}]
+},
+{
+  timestamps: true
+});
 
- module.exports = mongoose.model('Playlist', PlaylistSchema);
+module.exports = mongoose.model('Playlist', PlaylistSchema);
