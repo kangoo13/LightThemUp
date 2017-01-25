b72b78e620549040ff615970b594ed4e8a6aed9e
Tanguy DEVOS <contact@tan-devos.fr>
2017-01-25 23:26:46 +0100

    fix error for delete comment from song + apiDoc

--- API/apidoc/api_project.js
+++ API/apidoc/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-01-25T09:58:53.635Z",
+    "time": "2017-01-25T22:26:33.461Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
