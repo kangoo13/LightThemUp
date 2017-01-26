2f4213f6270ec96dbe72baa638418948b6ee7db3
Tanguy DEVOS <contact@tan-devos.fr>
2017-01-26 01:01:43 +0100

    frontend fixes

--- Frontend/public/js/controllers.js
+++ Frontend/public/js/controllers.js
@@ -522,8 +522,8 @@ app.controller('LastSongsSideBlockController', ['$scope', 'UserService', 'SongSe
 	});
 }]);
 
-app.controller('ShopController', ['$scope', '$cookies', 'SongService', 'PaypalService', 'toastr', '$window',
-function ($scope, $cookies, SongService, PaypalService, toastr, $window) {
+app.controller('ShopController', ['$scope', '$cookies', 'SongService', 'PaypalService', 'UserService', 'toastr', '$window',
+function ($scope, $cookies, SongService, PaypalService, UserService, toastr, $window) {
 
 	var vm = this;
 	vm.dataLoading = true;
@@ -536,6 +536,16 @@ function ($scope, $cookies, SongService, PaypalService, toastr, $window) {
 	SongService.GetNewSongs(3).then(function (response) {
 		$scope.newSongs = response;
 		vm.dataLoading = false;
+	});
+
+	UserService.Account($cookies.get('id')).then(function (response) {
+		$scope.bought = false;
+		$scope.user = response;
+
+		for(var i = 0; i < response.songs.length; i++) {
+			if (response.songs[i].slug == $routeParams.slug)
+			$scope.bought = true;
+		}
 	});
 
 	vm.buySong = buySong;
@@ -612,7 +622,7 @@ function ($scope, $routeParams, $cookies, PaypalService, UserService, $location,
 		else {
 			vm.dataLoading = false;
 			toastr.success("Merci pour votre achat ! :)");
-			$window.location.href = frontendUrl + "mes-musiques";
+			$window.location.href = frontendUrl + "/mes-musiques";
 		}
 	});
 }]);
